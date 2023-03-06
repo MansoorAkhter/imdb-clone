d7500f13a284ee12452fcc2d91b5d35e2c494dc3
Mansoor Akhter <mansoor21st@Gmail.com>
2023-03-06 19:52:42 +0500

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+    return <div className="max-w-6xl mx-auto space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+
+        <p>Lorem ipsum dolor sit amet. Sit consectetur consectetur non deleniti blanditiis qui atque atque quo nesciunt nostrum ut ipsa nesciunt. Aut totam nesciunt et dolorem omnis id cumque sapiente et delectus nulla id commodi ipsum in sapiente incidunt sit corporis voluptatibus. </p>
+
+        <p>Qui officia voluptatibus sit nesciunt fugiat aut consectetur molestias ut optio dolorum est alias soluta nam voluptatibus vero. Aut perferendis voluptatem ad quaerat fugiat eum odio consequatur ut quia repellat et iure odit qui voluptas tempore ut laborum ratione?</p>
+
+        <p>Lorem ipsum dolor sit amet. Sit consectetur consectetur non deleniti blanditiis qui atque atque quo nesciunt nostrum ut ipsa nesciunt. Aut totam nesciunt et dolorem omnis id cumque sapiente et delectus nulla id commodi ipsum in sapiente incidunt sit corporis voluptatibus. Qui officia voluptatibus sit nesciunt fugiat aut consectetur molestias ut optio dolorum est alias soluta nam voluptatibus vero. Aut perferendis voluptatem ad quaerat fugiat eum odio consequatur ut quia repellat et iure odit qui voluptas tempore ut laborum ratione?</p>
+    </div>;
+};
+
+export default About;
