8d3b05437add23b20ab7bfc0cb3eeb233b406e45
Mansoor Akhter <mansoor21st@Gmail.com>
2023-03-06 15:54:54 +0500

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'IMDB-clone',
+  description: 'This is the IMDB clone website',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
