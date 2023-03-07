624594656179f8e4a37c7e3f5263b003281271a8
Mansoor Akhter <mansoor21st@Gmail.com>
2023-03-07 15:23:05 +0500

    add loading screen

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+
+const loading = () => {
+    return <div className="flex justify-center">
+        <img className="h-96" src="spinner.svg" alt="loading spinner svg" />
+    </div>;
+};
+
+export default loading;
