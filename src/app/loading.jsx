01917ab2cfccab8250b238cce519b6ddc80948c8
Mansoor Akhter <mansoor21st@Gmail.com>
2023-03-08 17:32:41 +0500

    fix loading spinner

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
 
 const loading = () => {
     return <div className="flex justify-center">
-        <img className="h-96" src="spinner.svg" alt="loading..." />
+        {/* <img className="h-96" src="spinner.svg" alt="loading..." /> */}
+        <Image width={384} height={384} src="spinner.svg" alt="loading..." />
+
     </div>;
 };
 
